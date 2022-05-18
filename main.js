790272a36fd4f19e5af650e9be59bd8c6465c849
Micael Angelo Sabadin Presotto <50453035+MicaPresotto@users.noreply.github.com>
2022-05-18 10:20:35 -0300

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,61 @@
+window.addEventListener('scroll', onScroll)
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showbacktotop()
+  activateMenuCurrentSection(home)
+  activateMenuCurrentSection(services)
+  activateMenuCurrentSection(about)
+  activateMenuCurrentSection(contact)
+}
+function activateMenuCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(
+  '#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about .content, #about img'
+)
+
+function showbacktotop() {
+  if (scrollY > 500) {
+    backtotop.classList.add('show')
+  } else {
+    backtotop.classList.remove('show')
+  }
+}
